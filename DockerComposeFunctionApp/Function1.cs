83001cfb57ac78304315dc833e04823039721363
Lee Blundell <lee.blundell@openwrks.com>
2021-03-22 14:37:44 +0000

    init

---
+++ DockerComposeFunctionApp/Function1.cs
@@ -0,0 +1,23 @@
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace DockerComposeFunctionApp
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            return new OkObjectResult("hello");
+        }
+    }
+}
+
